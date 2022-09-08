0edf44bd4c3c1c67f2e71c32f8e276fd4af85ef0
João Vitor Estevam <joaophvt@gmail.com>
2022-09-08 17:06:19 -0300

    Teste Javascript

---
+++ javascript-2.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')();
+
+const weight = prompt('what s your weight? ')
+
+const name = prompt('Whats your name? ')
+
+const height = prompt('What is your height? ')
+
+const hospitalization = prompt('when you were hospitalized? ')
+
+const diabetic = prompt('you are diabetic? ')
